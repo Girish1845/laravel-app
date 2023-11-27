c08d3ec6ce614fb535d7b70714e1c6e73a3f7a11
Girish Nasre <girishnasre245@gmail.com>
2023-11-28 03:50:18 +0530

    Add routes for veggies

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +11,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/veggies/{sabji}', function ($sabji) {
+
+    return $sabji;
+})->whereIn('sabji', ['baigan', 'bhindi', 'aaloo', 'gobhi']);
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+Route::get('/hello', function () {
+    return view('veggies');
+});
+
+// On route '/veggies 'a chart should be seen , with name and price of veggies
+Route::get('/veggies', function () {
+    return view('table');
 });
