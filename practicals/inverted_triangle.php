7c0f6ec1fb99fb83e96e4a76c4ce7cb65449f758
Girish Nasre <girishnasre245@gmail.com>
2023-11-28 03:16:02 +0530

    inverted triangle(Readable and Maintainable)

--- practicals/inverted_triangle.php
+++ practicals/inverted_triangle.php
@@ -2,7 +2,7 @@
 $rows = 5; // Change this value to create a different-sized inverted triangle
 
 for ($i = $rows; $i >= 1; $i--) {
-    for ($j = 1; $j <= $i; $j++) {
+    for ($j = 1; $j <= $i; $j++) {//iner loop
         echo "* ";
     }
     echo "\n";
